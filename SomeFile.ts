cd06e2cf64304a2d6f4960748b085e98457185be
Jonas Sourlier <jonas.sourlier@yolp.ch>
2023-02-25 21:27:50 +0100

    initial

---
+++ SomeFile.ts
@@ -0,0 +1,10 @@
+import { setLogLevel } from 'firebase/app';
+
+setLogLevel('debug');
+
+export class SomeClass {
+
+    public static test() {
+        return true;
+    }
+}
---
+++ Test.test.ts
@@ -0,0 +1,5 @@
+import { SomeClass } from './SomeFile';
+
+it("should return February as last month on March 29", async () => {
+    SomeClass.test();
+})
